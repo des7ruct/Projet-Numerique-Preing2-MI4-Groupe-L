fab9c05f0b12d880aef24b685d9ca7c26efd1d36
des7ruct <canaxel05@gmail.com>
2026-05-07 09:59:43 +0200

    Add files via upload
    ajout du fichier python pour le premier exercice de code

---
+++ OndePlane1d4L.py
@@ -0,0 +1,32 @@
+# Importations :
+
+from numpy import pi, exp, sqrt, real, imag, zeros, linspace
+
+import matplotlib.pyplot
+
+
+# Constantes :
+
+I = sqrt(-1 + 0j)
+
+
+# Fonctions :
+
+def PlaneWave(amp, k, omega, x, t) :
+    """
+    Fonction prenant en argument quatre paramètres :
+        - amp : 
+        - k : 
+        - omega : 
+        - x : 
+        - t : 
+    Et renvoyant une onde plane à 1d
+    """
+    
+
+
+    return
+    
+    
+b = sqrt(-1 + 0j)
+print(imag(a), imag(b))
