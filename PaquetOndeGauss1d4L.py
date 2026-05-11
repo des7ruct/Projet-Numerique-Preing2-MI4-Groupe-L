d7a4bdcc8a6ec4d0ddec85aebbd997913e68ab86
des7ruct <canaxel05@gmail.com>
2026-05-11 09:58:58 +0200

    Add constants and define GaussWP function
    Add constants and GaussWP function definition

--- PaquetOndeGauss1d4L.py
+++ PaquetOndeGauss1d4L.py
@@ -8,3 +8,24 @@ import matplotlib.pyplot
 # Constantes :
 
 I = sqrt(-1 + 0j)
+HBAR : float = 1.05457182 * 10 ** (-34)
+M : float = 9.1093837 * 10 ** (-31)
+
+# Fonctions :
+
+def GaussWP() :
+    """
+    Parametres :
+        k0 : 
+
+        a : 
+
+        x : 
+            position
+        t : 
+            temps
+    
+    Valeur de retour :
+        paquet d ondes gaussien psi(x, t)
+    """
+    return
