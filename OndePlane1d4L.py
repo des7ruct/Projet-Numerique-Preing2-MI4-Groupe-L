31ccbe56a84b21da9e45cf17b6db074fed453572
des7ruct <canaxel05@gmail.com>
2026-06-18 14:45:46 +0200

    Setting up proper planewave function

--- OndePlane1d4L.py
+++ OndePlane1d4L.py
@@ -2,31 +2,70 @@
 
 from numpy import pi, exp, sqrt, real, imag, zeros, linspace
 
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
 
 
 # Constantes :
 
-I = sqrt(-1 + 0j)
+I = 1j
+
+
+
+# Classes :
+
+class Wave:
+
+    amp: float 
+    k: float
+    omega: float
+
 
 
 # Fonctions :
 
-def PlaneWave(amp, k, omega, x, t) :
-    """
-    Fonction prenant en argument quatre paramètres :
-        - amp : 
-        - k : 
-        - omega : 
-        - x : 
-        - t : 
-    Et renvoyant une onde plane à 1d
-    """
+def Verification(w: Wave) -> None {
+    if (w.amp <= 0) {
+        sys.exit("amp doit être strictement supérieur à 0")
+    }
+    
+    elif (w.k == 0) {
+        sys.exit("k ne doit pas être égal à 0.")
+    } 
+
+    elif (w.omega <= 0) {
+        sys.exit("omega doit être strictement supérieur à 0.")
+    } 
+}
+
+
+def PlaneWave(w: Wave x: float, t: float) :
+    if (k <= 0) {
+        exit
+    } 
+    
+
+    return amp * exp(I * (k * x - omega * t))
     
 
 
-    return
-    
-    
-b = sqrt(-1 + 0j)
-print(imag(a), imag(b))
+
+
+# Code principal
+
+print("Saisir une amplitude : ")
+amp = float input()
+
+print("Saisir un vecteur d'onde : ")
+k = float input()
+
+print("Saisir une pulsation : ")
+omega = float input()
+
+MyWave = Wave(amp, k, omega)
+
+Verification(MyWave)
+
+
+fig, ax = plt.subplots()
+ax.plot(x, y)
+plt.show()
