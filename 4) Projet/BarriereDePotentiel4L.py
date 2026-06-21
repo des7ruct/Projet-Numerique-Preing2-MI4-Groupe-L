a9391545d5910508628383423cce0bc8a09b4309
des7ruct <canaxel05@gmail.com>
2026-06-21 08:31:00 +0200

    Added potential barrier to graph

--- 4) Projet/BarriereDePotentiel4L.py
+++ 4) Projet/BarriereDePotentiel4L.py
@@ -71,6 +71,7 @@ ax.set_xlabel("Position x", fontsize = 10)
 ax.set_ylabel("Densité de probabilité |Psi|^2", fontsize = 10)
 ax.set_title("Evolution temporelle du paquet d'onde vers une barrière de potentiel", fontsize=12)
 ax.grid(True, linestyle=":", alpha=0.5)
+ax.axvspan(X_START, X_END, color="red", alpha=0.15, label="Barrière de potentiel")
 
 line_density, = ax.plot([], [], color = "black", linewidth = 2, label = r"|psi|^2")
 line_real, = ax.plot([], [], color = "blue", alpha = 0.3, label = "Re(psi)")
